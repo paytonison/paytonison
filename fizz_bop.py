966eff5ec494375c2e958dc7a5be68fcb0f77d22
Payton Ison <paytonison@icloud.com>
2025-07-09 01:27:45 -0600

    hehehe

--- fizz_bop.py
+++ fizz_bop.py
@@ -10,7 +10,7 @@ def sugma_ligma(limit: int = 100) -> None:
     for num in range(1, limit + 1):
         match (num % 3 == 0, num % 5 == 0):
             case (True, True):
-                print("sugma-ligma")
+                print("bofa")
             case (True, False):
                 print("sugma")
             case (False, True):
