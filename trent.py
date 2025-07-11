83494a14c34ca335a4748f4d3e67887df8ab2805
Payton Ison <paytonison@icloud.com>
2025-07-10 21:01:55 -0600

    update

--- trent.py
+++ trent.py
@@ -4,13 +4,25 @@ client = OpenAI()
 def get_response(prompt):
     response = client.responses.create(
         model="gpt-4.1",
-        input=prompt
+        input=prompt,
     )
     return response.output_text
 
-if __name__ == "__main__":
-    prompt = '''You are an assistant that only answers questions in Old English using Latin characters.
-    What is the capital of France?
-    '''
-    response = get_response(prompt)
+def main():
+    input = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        },
+        {
+            "role": "user",
+            "content": "What is the capital of France?"
+        }
+    ]
+
+    response = get_response(input)
     print(response)
+
+if __name__ == "__main__":
+    main()
+
