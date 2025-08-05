7c2d7a3f6b7058c6d7196ed13e63065193c0c743
Payton Ison <paytonison@icloud.com>
2025-08-04 22:36:28 -0600

    butts

--- bofa.py
+++ bofa.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python3
-
 def sugma_ligma(limit: int = 100) -> None:
-    """
-    Print numbers from 1‒limit replacing:
-     - multiples of 3 with "sugma"
-     - multiples of 5 with "ligma"
-     - multiples of both with "sugma-ligma"
-    """
     for num in range(1, limit + 1):
         match (num % 3 == 0, num % 5 == 0):
             case (True, True):
