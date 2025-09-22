d4bb17ebe61c01440c7407d03eb4a520addd0315
Payton Ison <paytonison@icloud.com>
2025-09-21 23:32:16 -0600

    butts

---
+++ butts.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    print("butts")
+
+def butts():
+    return "butts"
+def more_butts():
+    return "more butts"
+def even_more_butts():
+    return "even more butts"
+def yet_more_butts():
+    return "yet more butts"
+
