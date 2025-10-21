36c402ad8c797521fcf92c7decaa615c0db99cb5
Payton Ison <paytonison@icloud.com>
2025-10-20 22:40:20 -0600

    Recursive critical reasoning paper added

--- papers/.nvim-state/nvim/lazy/pkg-cache.lua
+++
@@ -1,1 +0,0 @@
-return {version=12,pkgs={{dir="/Users/paytonison/.local/share/nvim/lazy/plenary.nvim",name="plenary.nvim",source="lazy",file="community",spec={"nvim-lua/plenary.nvim",lazy=true,},},},}
